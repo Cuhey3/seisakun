9560680521fcc0626e78f3f98be6bcbe2e98a150
DOIF0568 <DOIF0568@AFF160.atena.local>
2015-06-16 19:11:40 +0900

    !groupby.zipを新たに追加　無効フラグの立っていないレコード同士で郵便番号でグループ化して印をつけます（列右端）

--- src/main/java/mycode/converter/bean/Groupby.java
+++ src/main/java/mycode/converter/bean/Groupby.java
@@ -1,6 +1,7 @@
 package mycode.converter.bean;
 
 import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
@@ -13,47 +14,41 @@ import org.springframework.stereotype.Component;
 public class Groupby {
 
     public void zip(@Body List<Map<String, String>> body, @Header("field") Field field) {
-        Iterator<Map<String, String>> itr1 = body.iterator();
-        LinkedHashSet<String> zipSet = new LinkedHashSet<>();
-        while (itr1.hasNext()) {
-            Map<String, String> next1 = itr1.next();
-            Iterator<Map<String, String>> itr2 = body.iterator();
-            boolean flag = false;
-            Integer sign = null;
-            String ng1 = next1.get("無効");
-            if (ng1 != null && !ng1.isEmpty()) {
-                continue;
-            }
-            String zip1 = next1.get("郵便番号");
-            if (zip1 == null) {
-                continue;
-            }
-            while (itr2.hasNext()) {
-                Map<String, String> next2 = itr2.next();
-                String ng2 = next2.get("無効");
-                if (ng2 != null && !ng2.isEmpty()) {
-                    continue;
-                }
-                if (flag) {
-                    String zip2 = next2.get("郵便番号");
-                    if (zip2 != null) {
-                        if (zip1.equals(zip2)) {
-                            if (sign == null) {
-                                zipSet.add(zip1);
-                                sign = zipSet.size();
-                                next1.put("郵番グループ", sign + "");
-                            }
-                            next2.put("郵番グループ", sign + "");
+        Iterator<Map<String, String>> iterator = body.iterator();
+        LinkedHashMap<String, Integer> map = new LinkedHashMap<>();
+        LinkedHashSet<String> set = new LinkedHashSet<>();
+        while (iterator.hasNext()) {
+            Map<String, String> next = iterator.next();
+            if (next.containsKey("無効") && !next.get("無効").isEmpty()) {
+
+            } else {
+                String zip = next.get("郵便番号");
+                if (zip != null && !zip.isEmpty()) {
+                    if (map.containsKey(zip)) {
+                        if (!set.contains(zip)) {
+                            set.add(zip);
+                            map.put(zip, set.size());
                         }
-                    }
-                } else {
-                    if (next1.equals(next2)) {
-                        flag = true;
+                    } else {
+                        map.put(zip, null);
                     }
                 }
             }
         }
-        
+        iterator = body.iterator();
+        while (iterator.hasNext()) {
+            Map<String, String> next = iterator.next();
+            if (next.containsKey("無効") && !next.get("無効").isEmpty()) {
+            } else {
+                String zip = next.get("郵便番号");
+                if (zip != null && !zip.isEmpty()) {
+                    Integer get = map.get(zip);
+                    if (get != null) {
+                        next.put("郵番グループ", map.get(zip) + "");
+                    }
+                }
+            }
+        }
         field.addUnique("郵番グループ");
     }
 }
